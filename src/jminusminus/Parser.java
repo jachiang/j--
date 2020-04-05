6f9d669db8a2d080e99b919c1515c58f3624f5bb
James Chiang <james.chiangwu@gmail.com>
2020-04-05 19:35:31 +0200

    Manual parser: Add double literal

--- src/jminusminus/Parser.java
+++ src/jminusminus/Parser.java
@@ -1417,6 +1417,8 @@ public class Parser {
             return new JLiteralChar(line, scanner.previousToken().image());
         } else if (have(STRING_LITERAL)) {
             return new JLiteralString(line, scanner.previousToken().image());
+        } else if (have(DOUBLE_LITERAL)) {
+            return new JLiteralDouble(line, scanner.previousToken().image());
         } else if (have(TRUE)) {
             return new JLiteralTrue(line);
         } else if (have(FALSE)) {
